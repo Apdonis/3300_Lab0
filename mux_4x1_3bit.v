d2ce0631074a345b7ad59019c41b5cc3a4219939
Apdonis <158244629+Apdonis@users.noreply.github.com>
2024-01-30 22:58:05 -0800

    Update mux_4x1_3bit.v

--- mux_4x1_3bit.v
+++ mux_4x1_3bit.v
@@ -28,6 +28,6 @@ output m1,m2,m3
     wire g3,g4,g5;
     mux_2x1_3bit N0 (.x0(x1),.y0(y1),.x1(x2),.y1(y2),.x2(x3),.y2(y3),.s0(s0),.f0(g0),.f1(g1),.f2(g2));
     mux_2x1_3bit N1 (.x0(z1),.y0(w1),.x1(z2),.y1(w2),.x2(z3),.y2(w3),.s0(s0),.f0(g3),.f1(g4),.f2(g5));
-    mux_2x1_3bit N3 (.x0(g0),.y0(g3),.x1(g1),.y1(g4),.x2(g3),.y2(g5),.s0(s1),.f0(m1),.f1(m2),.f2(m3));
+    mux_2x1_3bit N3 (.x0(g0),.y0(g3),.x1(g1),.y1(g4),.x2(g2),.y2(g5),.s0(s1),.f0(m1),.f1(m2),.f2(m3));
 
 endmodule
